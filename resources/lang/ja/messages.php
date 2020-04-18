e0175a225752ec06b2cec7129c2f10974445af6c
Takayuki-Takita <takayukihalifax@gmail.com>
2020-04-18 02:47:12 +0000

    PHP/Laravel 12 課題

---
+++ resources/lang/ja/messages.php
@@ -0,0 +1,9 @@
+<? php
+
+return [
+    'Login' => 'ログイン',
+    'E-mail Adress' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'Remember Me' => 'ログイン情報を記憶する',
+    ];
+    
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -36,7 +36,27 @@
 
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
+                            @guest
+                            <li><a class="nav-link" href="{{ route('login') }}"></a></li>
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbardropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclic="evevt.preventDefault();
+                                        document.getElementBy Id('logout-form').submit();">
+                                        {{ __('logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
                         </ul>
+
                     </div>
                 </div>
             </nav>
