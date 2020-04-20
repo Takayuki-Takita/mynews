fd799b839730747686e6704a282108f967ab9d0d
Takayuki-Takita <takayukihalifax@gmail.com>
2020-04-20 12:58:47 +0000

    PHP/Laravel 14 課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -17,25 +17,19 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">name</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">gender</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">hobby</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">introdaction</label>
+                        <label class="col-md-2" for="title">画像</label>
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
                         </div>
@@ -43,7 +37,6 @@
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
- 
             </div>
         </div>
     </div>
