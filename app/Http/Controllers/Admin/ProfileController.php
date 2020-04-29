a631f6aaed9a139e85b7acd7be21f4e95b107d29
Takayuki-Takita <takayukihalifax@gmail.com>
2020-04-29 02:44:15 +0000

    PHP/Laravel 14　課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,18 +5,41 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
     public function add()
     {
-        return view('admin.profile.create/admin/profile/create');
+        return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
         \Log::debug(__LINE__.' '.__FILE__);
-        return view('admin.profile.create');
+        
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
+        $form = $request->all();
+
+      // formに画像があれば、保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+          $profile->image_path = null;
+      }
+
+      unset($form['_token']);
+      unset($form['image']);
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+        
+        return redirect('admin/profile/create');
+        
     }
     
     public function edit()
