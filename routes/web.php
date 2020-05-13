b664e3d3deb4006a485cfd75abbb78a7c11489ae
Takayuki-Takita <takayukihalifax@gmail.com>
2020-05-13 12:56:52 +0000

    PHP/Laravel 18 課題

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('news/delete', 'Admin\NewsController@delete');
     Route::get('profile/delete', 'Admin\ProfileController@delete');
     Route::get('profile', 'Admin\ProfileController@index');
+    Route::get('/', 'NewsController@index');
+    Route::get('profile', 'ProfileController@index');
 
 });
 
